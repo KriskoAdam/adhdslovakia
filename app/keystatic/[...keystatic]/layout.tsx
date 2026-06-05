ce93c8d9e410a5ad28d5a3c7cbcfb06b0b25ae27
KriskoAdam <adam.krisko@yahoo.com>
2026-06-05 03:31:10 +0200

    fix: keystatic routing and layout

---
+++ app/keystatic/[...keystatic]/layout.tsx
@@ -0,0 +1,17 @@
+export const metadata = {
+  title: 'Keystatic Admin',
+};
+
+export default function KeystaticLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    
+      
+        {children}
+      
+    
+  );
+}
